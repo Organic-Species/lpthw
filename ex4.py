01c2d472a4bd75ecf85572ea4f55dcd8939d1285
Jarrel Thomas <jarrel.thomas@gmail.com>
2022-01-20 00:35:15 -0500

    main

--- ex4.py
+++ ex4.py
@@ -8,5 +8,9 @@ carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers / cars_driven
 
 
-print("There
-
+print("There are", cars, "cars avalible.")
+print("There are only", drivers, "drivers avalible.")
+print("There will be", cars_not_driven, "empty cars today.")
+print("We can transport", carpool_capacity, "people today.")
+print("We have", passengers, "to carpool today.")
+print("We need to put about", average_passengers_per_car, "in each car.")
